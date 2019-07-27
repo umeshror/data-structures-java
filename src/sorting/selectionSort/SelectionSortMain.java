c2fa14f5a8e63cc2af74f9804af3c2f013722ffa
umeshror <sarukumesh@gmail.com>
2019-07-28 00:32:00 +0530

    selectionSort added

---
+++ src/sorting/selectionSort/SelectionSortMain.java
@@ -0,0 +1,17 @@
+package sorting.selectionSort;
+
+public class SelectionSortMain {
+
+	public static void main(String[] args) {
+		int array[] = {10, 3, 2, 5, 8, 4, 3, 1, 2, 9, 7, 8};
+		
+		System.out.println("User entered Array: ");
+		SelectionSort.printArray(array);
+		
+		SelectionSort.selectionSort(array);
+		
+		System.out.println("\n\nAfter sorting: ");
+		SelectionSort.printArray(array);
+	}
+
+}
