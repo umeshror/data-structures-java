9050dfbebf95f40133da7caa11147d52eed007d1
umeshror <sarukumesh@gmail.com>
2019-08-03 11:14:12 +0530

    WeightedNode added

--- src/node/WeightedNode.java
+++ src/node/WeightedNode.java
@@ -5,84 +5,84 @@ import java.util.*;
 import mst.DisjointSet;
 
 public class WeightedNode implements Comparable<WeightedNode> {
-	public String name;
+    public String name;
 
-	private ArrayList<WeightedNode> neighbors = new ArrayList<WeightedNode>();
-	private HashMap<WeightedNode, Integer> weightMap = new HashMap<>();
-	private boolean isVisited = false;
-	private WeightedNode parent;
-	private int distance;
-	private DisjointSet set; //used in DisjointSet Algorithm
+    private ArrayList<WeightedNode> neighbors = new ArrayList<WeightedNode>();
+    private HashMap<WeightedNode, Integer> weightMap = new HashMap<>();
+    private boolean isVisited = false;
+    private WeightedNode parent;
+    private int distance;
+    private DisjointSet set; //used in DisjointSet Algorithm
 
-	public WeightedNode(String name) {
-		this.name = name;
-		distance = Integer.MAX_VALUE;
-	}
+    public WeightedNode(String name) {
+        this.name = name;
+        distance = Integer.MAX_VALUE;
+    }
 
-	public DisjointSet getSet() {
-		return set;
-	}
+    public DisjointSet getSet() {
+        return set;
+    }
 
-	public void setSet(DisjointSet set) { //used in DisjointSet Algorithm
-		this.set = set;
-	}
+    public void setSet(DisjointSet set) { //used in DisjointSet Algorithm
+        this.set = set;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public ArrayList<WeightedNode> getNeighbors() {
-		return neighbors;
-	}
+    public ArrayList<WeightedNode> getNeighbors() {
+        return neighbors;
+    }
 
-	public void setNeighbors(ArrayList<WeightedNode> neighbors) {
-		this.neighbors = neighbors;
-	}
+    public void setNeighbors(ArrayList<WeightedNode> neighbors) {
+        this.neighbors = neighbors;
+    }
 
-	public HashMap<WeightedNode, Integer> getWeightMap() {
-		return weightMap;
-	}
+    public HashMap<WeightedNode, Integer> getWeightMap() {
+        return weightMap;
+    }
 
-	public void setWeightMap(HashMap<WeightedNode, Integer> weightMap) {
-		this.weightMap = weightMap;
-	}
+    public void setWeightMap(HashMap<WeightedNode, Integer> weightMap) {
+        this.weightMap = weightMap;
+    }
 
-	public boolean isVisited() {
-		return isVisited;
-	}
+    public boolean isVisited() {
+        return isVisited;
+    }
 
-	public void setVisited(boolean isVisited) {
-		this.isVisited = isVisited;
-	}
+    public void setVisited(boolean isVisited) {
+        this.isVisited = isVisited;
+    }
 
-	public WeightedNode getParent() {
-		return parent;
-	}
+    public WeightedNode getParent() {
+        return parent;
+    }
 
-	public void setParent(WeightedNode parent) {
-		this.parent = parent;
-	}
+    public void setParent(WeightedNode parent) {
+        this.parent = parent;
+    }
 
-	public int getDistance() {
-		return distance;
-	}
+    public int getDistance() {
+        return distance;
+    }
 
-	public void setDistance(int distance) {
-		this.distance = distance;
-	}
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
 
-	@Override
-	public String toString() {
-		return name;
-	}
+    @Override
+    public String toString() {
+        return name;
+    }
 
-	@Override
-	public int compareTo(WeightedNode o) {
-		return this.distance - o.distance;
-	}
+    @Override
+    public int compareTo(WeightedNode o) {
+        return this.distance - o.distance;
+    }
 
 }
