a7243c0ee18903fe69bb8dd9ae79a5f9ce3c86a7
umeshror <sarukumesh@gmail.com>
2019-08-07 23:14:39 +0530

    LongestPalindromicSubstring added

---
+++ src/divideConquer/longestPalindromicSubstring/LongestPalindromicSubstring.java
@@ -0,0 +1,15 @@
+package divideConquer.longestPalindromicSubstring;
+/*
+ * We are given a string ’S’
+ * We need to find the length of its Longest Palindromic Substring
+ * Palindrome is a sting that reads the same backward as well as forward and can be of odd or even length
+ * A subsequence is a sequence that can be derived from another string by deleting some or no elements without changing the order of the remaining elements
+ * A Substring is a contiguous sequence of characters within a string
+ABCCBUA
+Output 4
+ans. BCCB
+
+ */
+public class LongestPalindromicSubstring {
+
+}
