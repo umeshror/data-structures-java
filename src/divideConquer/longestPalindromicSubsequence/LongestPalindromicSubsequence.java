56200c843b1781caeeed4e36fe6c9c671ce6733e
umeshror <sarukumesh@gmail.com>
2019-08-07 22:46:16 +0530

    longestPalindromicSubsequence added

---
+++ src/divideConquer/longestPalindromicSubsequence/LongestPalindromicSubsequence.java
@@ -0,0 +1,18 @@
+package divideConquer.longestPalindromicSubsequence;
+/*
+* We are given a string ’S’
+* We need to find the length of its Longest Palindromic Subsequence in the given ’S’ String
+* A subsequence is a sequence that can be derived from another string by deleting some or no elements without changing the order of the remaining elements
+* Palindrome is astinrg that reads the same backward as well as forward and can be of odd or even length
+
+e.g. 1
+Input: ‘ELRMENMET’
+Output: 5
+LPS is EMEME
+
+
+ */
+class LongestPalindromicSubsequence {
+
+
+}
