17ee7aedacd468b2cf7a54483679eb0b60b2ba19
umeshror <sarukumesh@gmail.com>
2019-08-08 06:45:03 +0530

    MinCostToReachLastCellIIn2DArray added

---
+++ src/divideConquer/minCostToReachLastCellIIn2DArray/MinCostToReachLastCellIIn2DArray.java
@@ -0,0 +1,11 @@
+package divideConquer.minCostToReachLastCellIIn2DArray;
+/*
+ * We are given a 2D matrix
+ * Accessing each cell have a. Cost associated with it
+ * We need. To start from (0,0) cell and go till (n-1, n-1) cell
+ * We can go only right or down cell from current cell
+ * Challange is to do the traversal in minimum cos
+ */
+public class MinCostToReachLastCellIIn2DArray {
+
+}
