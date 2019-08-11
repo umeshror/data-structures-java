762de704878d50bc955e76036b225d66e95e0d92
umeshror <sarukumesh@gmail.com>
2019-08-11 16:31:50 +0530

    NumberOfPathsToReachLastCellTD updated

--- src/dynamicProgramming/numberOfPathsToReachLastCell/NumberOfPathsToReachLastCellTD.java
+++ src/dynamicProgramming/numberOfPathsToReachLastCell/NumberOfPathsToReachLastCellTD.java
@@ -1,4 +1,12 @@
 package dynamicProgramming.numberOfPathsToReachLastCell;
+/*
+ * We are given a 2D matrix
+ * Accessing each cell have a. Cost associated with it
+ * We need. To start from (0,0) cell and go till (n-1, n-1) cell
+ * We are given total cost to reach end of array
+ * We can go only right or down cell from current cell
+ * Challenge is to find the number of ways to reach end of matrix with given ’total cost'
+ */
 
 public class NumberOfPathsToReachLastCellTD {
 
